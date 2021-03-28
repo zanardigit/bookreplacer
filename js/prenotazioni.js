b00f8e0a7590892adccfb811525e7cc8bc18ed81
ConteEmanuele <conteemanuele002@gmail.com>
2021-03-28 15:17:09 +0200

    Implementazione template

--- js/prenotazioni.js
+++ js/prenotazioni.js
@@ -1,146 +1,126 @@
+let tbody = document.querySelector("tbody");
+let template = document.querySelector('#temp1');
+
+
 fetch ("js/libri.json")
-    .then( response => response.json() )
-    .then( data => {
+.then( response => response.json() )
+.then( data => {
 
-    let array = data;
-    console.log(data);
-    for( let i = 0; i < data.length; i++ ){
-        let row = document.createElement("tr");
-        row.id = "riga" + (i + 1);
+console.log(data);
+for( let i = 0; i < data.length; i++ ){
 
-        immagini(i, data , row);
-        stato( i,data, row );
-        titolo( i, data, row );
-        anno( i, data, row );
-        azione( data[i].stato, row, i);
+  let clone = template.content.cloneNode(true);
 
-    document.getElementById("tabellaPrenotazioni").childNodes[1].appendChild(row);
-      }
+  var tr = clone.querySelector("tr");
+  tr.id += (i+1);
+
+  var td = clone.querySelectorAll("td");
+  
+  renderizzaImmagini( td[0], i, data[i].copertina );
+  caricaStatoLibro( td[1] ,i, data[i].stato );
+  caricaTitoloLibro( td[2], data[i].titolo )
+  caricaAnnoLibro( td[3], data[i].anno )
+  caricaAzioniPossibili( td[4], i, data[i].stato);
+
+  tbody.appendChild(clone);
+
+  }
 
 } )
 
-function immagini(i, libri, row){
+function renderizzaImmagini( td, i, copertina ){
+  let inp = td.querySelectorAll("input");
 
-    let td = document.createElement("td");
+  inp[0].id += (i+1)
+  inp[0].setAttribute( "onClick", "appareScompare(" + (i+1) + ")" )
+
+  inp[1].id += (i+1)
+  inp[1].setAttribute( "onClick", "appareScompare(" + (i+1) + ")" )
+  inp[1].src = copertina;
+}
+
+function caricaStatoLibro( td, i, stato ){
+  let small = td.querySelector("small")
+  small.id += (i+1);
+  if( stato == "In sospeso" || stato == "In prestito" || stato == "Restituito" ){
+    small.className = "badge badge-primary";
+  } else{
+      small.className = "badge badge-danger";
+  }
+  small.textContent = stato;
+}
+
+function caricaTitoloLibro( td, titolo ){
+  td.textContent = titolo
+}
+
+function caricaAnnoLibro( td, anno ){
+  td.textContent = anno
+}
+
+function caricaAzioniPossibili(td, i, stato){
+  if( stato == "In sospeso" ){
+
     let btn = document.createElement("input");
     btn.type = "button";
-    btn.id = "pulsante" + (i+1);
-    btn.setAttribute("onclick", ("appareScompare(" + (i+1) + ")"));
-    btn.className = "btn btn-info";
-    btn.style = "display: inline;";
-    btn.value = "Copertina"
+    btn.className = "btn btn-danger";
+    btn.id = "annulla" + (i+1);
+    btn.setAttribute("onclick", ("insospesoAnnulla(" + (i+1) + ")"));
+    btn.value = "ANNULLA"
     td.appendChild(btn);
 
-    let img = document.createElement("input");
-    img.type = "image";
-    img.id = "immagine" + (i+1);
-    img.setAttribute("onclick", ("appareScompare(" + (i+1) + ")"));
-    img.src = libri[i].copertina;
-    img.style = "display: none;";
-    td.appendChild(img);
-    row.appendChild(td);
+    let btn2 = document.createElement("input");
+    btn2.type = "button";
+    btn2.className = "btn btn-primary";
+    btn2.id = "riprenota" + (i+1);
+    btn2.setAttribute("onclick", ("Riprenota(" + (i+1) + ")"));
+    btn2.value = "RIPRENOTA";
+    btn2.style = "display: none;";
+    td.appendChild(btn2);
+
+    let btn3 = document.createElement("input");
+    btn3.type = "button";
+    btn3.className = "btn btn-danger";
+    btn3.id = "elimina" + (i+1);
+    btn3.setAttribute("onclick", ("Elimina(" + (i+1) + ")"));
+    btn3.value = "ELIMINA";
+    btn3.style = "display: none;";
+    td.appendChild(btn3);
 
 }
 
-function stato(i, libri, row){
+else if( stato == "Respinto" || stato == "Annullato" ){
 
-    let td = document.createElement("td");
-    let small = document.createElement("small");
-    small.id = "insospeso" + (i+1);
-    if( libri[i].stato == "In sospeso" || libri[i].stato == "In prestito" || libri[i].stato == "Restituito" ){
-        small.className = "badge badge-primary";
-    } else{
-        small.className = "badge badge-danger";
-    }
-    let stato = document.createTextNode( libri[i].stato );
-    small.appendChild(stato);
-    td.appendChild(small);
-    row.appendChild(td);
+    let btn = document.createElement("input");
+    btn.type = "button";
+    btn.className = "btn btn-danger";
+    btn.id = "annulla" + (i+1);
+    btn.setAttribute("onclick", ("insospesoAnnulla(" + (i+1) + ")"));
+    btn.value = "ANNULLA"
+    btn.style = "display: none;";
+    td.appendChild(btn);
+
+    let btn2 = document.createElement("input");
+    btn2.type = "button";
+    btn2.className = "btn btn-primary";
+    btn2.id = "riprenota" + (i+1);
+    btn2.setAttribute("onclick", ("Riprenota(" + (i+1) + ")"));
+    btn2.value = "PRENOTA";
+    td.appendChild(btn2);
+
+    let btn3 = document.createElement("input");
+    btn3.type = "button";
+    btn3.className = "btn btn-danger";
+    btn3.id = "elimina" + (i+1);
+    btn3.setAttribute("onclick", ("Elimina(" + (i+1) + ")"));
+    btn3.value = "ElIMINA";
+    td.appendChild(btn3);
 
 }
 
-function titolo(i, libri, row ){
-    let cell3 = document.createElement("td");
-        let cellText3 = document.createTextNode( libri[i].titolo );
-        cell3.appendChild(cellText3);
-        row.appendChild(cell3);
+else{
+    let p = document.createElement("p");
+    td.appendChild(p);
 }
 
-function anno(i, libri, row ){
-    let td = document.createElement("td");
-    let anno = document.createTextNode( libri[i].anno );
-    td.appendChild(anno);
-    row.appendChild(td);
-}
-
-function azione( stato, row, i ){
-
-    let td = document.createElement("td");
-
-    if( stato == "In sospeso" ){
-
-        let btn = document.createElement("input");
-        btn.type = "button";
-        btn.className = "btn btn-danger";
-        btn.id = "annulla" + (i+1);
-        btn.setAttribute("onclick", ("insospesoAnnulla(" + (i+1) + ")"));
-        btn.value = "ANNULLA"
-        td.appendChild(btn);
-
-        let btn2 = document.createElement("input");
-        btn2.type = "button";
-        btn2.className = "btn btn-primary";
-        btn2.id = "riprenota" + (i+1);
-        btn2.setAttribute("onclick", ("Riprenota(" + (i+1) + ")"));
-        btn2.value = "RIPRENOTA";
-        btn2.style = "display: none;";
-        td.appendChild(btn2);
-
-        let btn3 = document.createElement("input");
-        btn3.type = "button";
-        btn3.className = "btn btn-danger";
-        btn3.id = "elimina" + (i+1);
-        btn3.setAttribute("onclick", ("Elimina(" + (i+1) + ")"));
-        btn3.value = "ELIMINA";
-        btn3.style = "display: none;";
-        td.appendChild(btn3);
-
-    }
-
-    else if( stato == "Respinto" || stato == "Annullato" ){
-
-        let btn = document.createElement("input");
-        btn.type = "button";
-        btn.className = "btn btn-danger";
-        btn.id = "annulla" + (i+1);
-        btn.setAttribute("onclick", ("insospesoAnnulla(" + (i+1) + ")"));
-        btn.value = "ANNULLA"
-        btn.style = "display: none;";
-        td.appendChild(btn);
-
-        let btn2 = document.createElement("input");
-        btn2.type = "button";
-        btn2.className = "btn btn-primary";
-        btn2.id = "riprenota" + (i+1);
-        btn2.setAttribute("onclick", ("Riprenota(" + (i+1) + ")"));
-        btn2.value = "PRENOTA";
-        td.appendChild(btn2);
-
-        let btn3 = document.createElement("input");
-        btn3.type = "button";
-        btn3.className = "btn btn-danger";
-        btn3.id = "elimina" + (i+1);
-        btn3.setAttribute("onclick", ("Elimina(" + (i+1) + ")"));
-        btn3.value = "ElIMINA";
-        td.appendChild(btn3);
-
-    }
-
-    else{
-        let p = document.createElement("p");
-        td.appendChild(p);
-    }
-            
-    row.appendChild(td);
-            
 }
