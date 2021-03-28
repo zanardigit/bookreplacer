c00a551db1b6ea6648dbacd43208ed4b6d6a360e
ConteEmanuele <64140478+ConteEmanuele@users.noreply.github.com>
2021-03-28 19:07:06 +0200

    Aggiunto Codice Template

--- le-mie-prenotazioni.php
+++ le-mie-prenotazioni.php
@@ -55,6 +55,21 @@
 				</tr>
 			</th>
 		</table>
+		
+		<template id="temp1">
+			<tr id="riga">
+				<td>
+				<input type="button" id="pulsante" class="btn btn-info" value="Copertina">
+				<input type="image" id="immagine">
+				</td>
+
+				<td>	<small id="insospeso"></small>	</td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</template>
+		
 		<script src="js/prenotazioni.js"></script>
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
